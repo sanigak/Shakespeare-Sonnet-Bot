795c3d6bbde5fc7622abfcdfd95e2388ab725b24
Keith Saniga <sanigak@outlook.com>
2019-05-27 11:49:23 -0400

    this station is nearly fully operational

--- Shakespeare-Sonnet-Bot/Shakespeare_Sonnet_Bot.py
+++ Shakespeare-Sonnet-Bot/Shakespeare_Sonnet_Bot.py
@@ -11,7 +11,10 @@ ph = Phyme()
 
 def getRhymes(word):
 
-    listy = ph.get_perfect_rhymes(word)
+    try:
+        listy = ph.get_perfect_rhymes(word)
+    except:
+        return []
 
     num = len(listy)
 
@@ -28,6 +31,7 @@ def getRhymes(word):
                 outputList.append(str(output))
         iterator += 1
     return outputList
+
 
 def generateLine():
 
@@ -63,31 +67,45 @@ def generateRhymingLines():
     line1 = str(line1)
     lastWord1 = lastWord(line1)
 
-
     rhymes = []
+    rhymes = getRhymes(str(lastWord1))
 
 
-    try:
+    
+
+    while len(rhymes) < 40:
+        line1 = generateNewLine()
+        line1 = str(line1)
+        lastWord1 = lastWord(line1)
         rhymes = getRhymes(str(lastWord1))
-    except:
-        generateRhymingLines()
 
     try:
         rhymes.remove(lastWord1)
     except:
         print("blarn")
 
-    while len(rhymes) < 5:
-        line1 = generateNewLine()
-        rhymes = getRhymes(str(lastWord1))
 
     noRhyme = True
-
+    ity = 0
     while noRhyme:
-        print("LINE 1: " + line1)
+       
         line2 = generateNewLine()
         line2 = str(line2)
         lastWord2 = lastWord(line2)
+        ity+=1
+        if ity % 100 == 0:
+            print(ity)
+
+        if ity > 200:
+            line1 = generateNewLine()
+            line1 = str(line1)
+            lastWord1 = lastWord(line1)
+            rhymes = getRhymes(str(lastWord1))
+            try:
+                rhymes.remove(lastWord1)
+            except:
+                print("blarn")
+
 
 
         if str(lastWord2) in rhymes:
@@ -95,4 +113,98 @@ def generateRhymingLines():
     return [line1, line2]
 
 
-print(generateRhymingLines())
+
+def engine():
+
+    
+    usedRhymes = []
+
+    couplet1 = generateRhymingLines()
+    last1 = lastWord(couplet1[0])
+    rhymes1 = getRhymes(last1)
+    usedRhymes = rhymes1 + usedRhymes
+    
+    print(couplet1)
+
+
+    couplet2 = generateRhymingLines()
+    last2 = lastWord(couplet2[0])
+    while last2 in usedRhymes:
+        couplet2 = generateRhymingLines()
+        last2 = lastWord(couplet2[0])
+    rhymes2 = getRhymes(last2)
+    usedRhymes = usedRhymes + rhymes2
+
+    print(couplet2)
+
+    couplet3 = generateRhymingLines()
+    last3 = lastWord(couplet3[0])
+    while last3 in usedRhymes:
+        couplet3 = generateRhymingLines()
+        last3 = lastWord(couplet3[0])
+    rhymes3 = getRhymes(last3)
+    usedRhymes = usedRhymes + rhymes3
+    print(couplet3)
+
+
+    couplet4 = generateRhymingLines()
+    last4 = lastWord(couplet4[0])
+    while last4 in usedRhymes:
+        couplet4 = generateRhymingLines()
+        last4 = lastWord(couplet4[0])
+    rhymes4 = getRhymes(last4)
+    usedRhymes = usedRhymes + rhymes4
+    print(couplet4)
+
+
+    couplet5 = generateRhymingLines()
+    last5 = lastWord(couplet5[0])
+    while last5 in usedRhymes:
+        couplet5 = generateRhymingLines()
+        last5 = lastWord(couplet5[0])
+    rhymes5 = getRhymes(last5)
+    usedRhymes = usedRhymes + rhymes5
+    print(couplet5)
+
+
+    couplet6 = generateRhymingLines()
+    last6 = lastWord(couplet6[0])
+    while last6 in usedRhymes:
+        couplet6 = generateRhymingLines()
+        last6 = lastWord(couplet6[0])
+    rhymes6 = getRhymes(last6)
+    usedRhymes = usedRhymes + rhymes6
+    print(couplet6)
+
+
+    couplet7 = generateRhymingLines()
+    last7 = lastWord(couplet7[0])
+    while last7 in usedRhymes:
+        couplet7 = generateRhymingLines()
+        last7 = lastWord(couplet7[0])
+    rhymes7 = getRhymes(last7)
+    usedRhymes = usedRhymes + rhymes7
+    print(couplet7)
+
+    print(couplet1[0])
+    print(couplet2[0])
+    print(couplet1[1])
+    print(couplet2[1])
+
+    print(couplet3[0])
+    print(couplet4[0])
+    print(couplet3[1])
+    print(couplet4[1])
+
+    print(couplet5[0])
+    print(couplet6[0])
+    print(couplet5[1])
+    print(couplet6[1])
+
+    print(couplet7[0])
+    print(couplet7[1])
+
+
+
+
+engine()
